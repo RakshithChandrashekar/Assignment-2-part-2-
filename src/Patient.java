a8329a04bfe2bbedd31ebfa9287f8adac16a133b
RakshithChandrashekar <chandrashekar.r@northeastern.edu>
2021-02-22 16:54:01 -0500

    Assignment 2 part 2

---
+++ src/Patient.java
@@ -0,0 +1,154 @@
+
+import java.util.ArrayList;
+import java.util.Iterator;
+public class Patient {
+    ArrayList<vital_signs> pt=new ArrayList<vital_signs>();
+    private Iterator<vital_signs> vs;
+    boolean isPatientNormal(vital_signs vs){
+        float age=vs.age;
+        int respiratoty_rate=vs.respiratoty_rate;
+        int heart_rate=vs.heart_rate;
+        int blood_pressure=vs.blood_pressure;
+        float Kg_weight=vs.Kg_weight;
+        float Lb_weight=vs.Lb_weight;
+        boolean result;
+        if(age==0)
+        {
+            if((respiratoty_rate>=30 & respiratoty_rate<=50)&(heart_rate>=120 & heart_rate<=160)&(blood_pressure>=50 & blood_pressure<=70)&(Kg_weight>=2 & Kg_weight<=3)&(Lb_weight>=4.5 & Lb_weight<=7))
+            {
+                result =true;
+            }
+            else
+            {
+                result =(false);
+            }                        
+        }
+        else if (age>0 & age<1)
+        {
+            if((respiratoty_rate>=20 & respiratoty_rate<=30)&(heart_rate>=80 & heart_rate<=140)&(blood_pressure>=70 & blood_pressure<=100)&(Kg_weight>=4 & Kg_weight<=10)&(Lb_weight>=9 & Lb_weight<=22))
+            {
+                result =(true);
+            }
+            else
+            {
+                result =(false);
+            }
+            
+        }
+        else if (age>=1 & age<3)
+        {
+            if((respiratoty_rate>=20 & respiratoty_rate<=30)&(heart_rate>=80 & heart_rate<=130)&(blood_pressure>=80 & blood_pressure<=110)&(Kg_weight>=10 & Kg_weight<=14)&(Lb_weight>=22 & Lb_weight<=31))
+            {
+                result =(true);
+            }
+            else
+            {
+                result =(false);
+            }
+            
+        }
+        else if (age>=3 & age<6)
+        {
+            if((respiratoty_rate>=20 & respiratoty_rate<=30)&(heart_rate>=80 & heart_rate<=120)&(blood_pressure>=80 & blood_pressure<=110)&(Kg_weight>=14 & Kg_weight<=18)&(Lb_weight>=31 & Lb_weight<=40))
+            {
+                result =(true);
+            }
+            else
+            {
+                result =(false);
+            }
+            
+        }
+        else if (age>=6 & age<13)
+        {
+            if((respiratoty_rate>=20 & respiratoty_rate<=30)&(heart_rate>=70 & heart_rate<=110)&(blood_pressure>=80 & blood_pressure<=120)&(Kg_weight>=20 & Kg_weight<=42)&(Lb_weight>=41 & Lb_weight<=92))
+            {
+                result =(true);
+            }
+            else
+            {
+                result =(false);
+            }
+            
+        }
+        else
+        {
+            if((respiratoty_rate>=12 & respiratoty_rate<=20)&(heart_rate>=55 & heart_rate<=105)&(blood_pressure>=50)&(Kg_weight>50)&(Lb_weight>110))
+            {
+                result =(true);
+            }
+            else
+            {
+                result =(false);
+            }
+            
+        } 
+        return result;
+    }
+    
+    void history(vital_signs vs){
+        int a;
+        vital_signs b;        
+        pt.add(vs);
+        a=pt.size();
+        for(int i=0;i<a;i++)
+        {
+            b=pt.get(i);
+        }
+    }
+    vital_signs last_vitalsign(String name1)
+    {       
+        int a;
+        vital_signs b;
+        vital_signs vs = null;
+        a=pt.size();
+        for(int i=0;i<a;i++)
+        {
+            b=pt.get(i);
+            if(b.name.matches(name1))
+            {
+              vs=pt.get(a-1);  
+            }
+        }
+        return vs;        
+    }
+    void details(String name){
+        String abc=name;
+        int a=pt.size();
+        vital_signs b;
+        int count=1;
+        int count2=0;
+        System.out.println(" ");
+        System.out.println("name : "+abc);
+        for(int i=0;i<a;i++)
+        {
+            System.out.println(" "); 
+            b=pt.get(i);
+            if(b.name.matches(abc)){  
+            count2++;
+            System.out.println("vital sign "+count);
+            count++;
+            if (b.age>=1){            
+            System.out.println("age : "+b.age+ " year");
+            }
+            else
+            {
+               int c = (int) (b.age/0.0833334);
+               System.out.println("age : "+c+ " months"); 
+            }
+            System.out.println("respiratory rate : "+b.respiratoty_rate);
+            System.out.println("heart rate : "+b.heart_rate);
+            System.out.println("blood pressure : "+b.blood_pressure);
+            System.out.println("weight in kilograms : "+b.Kg_weight);
+            System.out.println("weight in pounds : "+b.Lb_weight);   
+            System.out.println(" "); 
+            }
+            
+        }
+        if(count2==0)
+        {
+            System.out.println("invalid patient name");
+        }
+        
+    }
+}
---
+++ src/vital_signs.java
@@ -0,0 +1,96 @@
+
+import java.util.Scanner;
+
+public class vital_signs {
+    String name;
+    float age;
+    int respiratoty_rate;
+    int heart_rate;
+    int blood_pressure;
+    float Kg_weight;
+    float Lb_weight;
+    void vital_signs()
+    {
+        Scanner x = new Scanner(System.in);
+        System.out.println(" ");
+        System.out.println("enter the name");
+        name=x.next();
+        System.out.println("enter 1 to input the age of the patients in months\nenter 2 to input the age of the patients in years");
+        int a= x.nextInt();
+        if(a==1)
+        {
+            System.out.println("enter the age of the patient in months : ");
+            age =x.nextFloat();
+            if (age>0)
+            {
+                age=(float) (age*0.0833334);
+            }
+            
+        }
+        else{
+            System.out.println("enter the age of the patient in years : ");
+            age =x.nextFloat();            
+        }
+        System.out.println("enter the respiratory rate : ");
+        respiratoty_rate =x.nextInt();
+        System.out.println("enter the heart rate : ");
+        heart_rate =x.nextInt();        
+        System.out.println("enter blood pressure : ");
+        blood_pressure =x.nextInt();
+        System.out.println("enter the weight in kelogram");
+        Kg_weight= x.nextInt();
+        System.out.println("enter the weight pounds");
+        Lb_weight= x.nextInt();  
+        System.out.println(" ");
+    }
+    public static void main(String[] args)
+    {
+        int value=100;
+        Patient p1= new Patient();
+        while(value!=0)
+        {
+            System.out.println("Enter 1 to insert the vital sign of a patient");
+            System.out.println("enter 2 to check is patient is normal");
+            System.out.println("enter 3 to check the history of patients vital sign");
+            System.out.println("enter 0 to exit");
+            Scanner x = new Scanner(System.in);
+            value=x.nextInt();
+            
+            switch (value) {
+                case 1:
+                    vital_signs vs=new vital_signs();
+                    vs.vital_signs();
+                    p1.history(vs);
+                break;
+                case 2:
+                    System.out.println(" ");
+                    System.out.println("enter the name of the patient");
+                    String name1=x.next();
+                    vital_signs vm;
+                    vm=p1.last_vitalsign(name1);
+                    if(vm==null)
+                    {
+                        System.out.println("is patient normal : false(invalid patient name)");
+                        System.out.println(" ");
+                    }
+                    else{
+                    Patient p =new Patient();
+                    boolean result=p.isPatientNormal(vm);                    
+                    System.out.println("is patient normal : "+result);
+                    System.out.println(" ");
+                    }
+                break;
+                case 3:
+                    System.out.println(" ");
+                    System.out.println("enter the name of the patient");
+                    String name=x.next();
+                    p1.details(name);
+                break;
+                
+            }
+            
+        }
+        
+    }
+    
+}
